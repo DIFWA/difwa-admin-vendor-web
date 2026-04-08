3a299b28321d7e5f2acff6cb5dd5ccf2f686c35d
anushk2026a <anushk2026a@gmail.com>
2026-04-08 11:21:39 +0530

    Update useSocketStore.js

--- data/store/useSocketStore.js
+++ data/store/useSocketStore.js
@@ -4,7 +4,7 @@ import useOrderStore from './useOrderStore';
 import useProductStore from './useProductStore';
 import useNotificationStore from './useNotificationStore';
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5000';
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'https://api.difwa.com';
 
 const useSocketStore = create((set, get) => ({
     socket: null,
